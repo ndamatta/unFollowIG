211bbb69a87f626e9c5e9383fd0e7b49f61c8b5e
ndamatta <damatta.natanael@gmail.com>
2025-05-15 19:53:24 -0300

    rename and change logic for burgerbutton

--- public/js/burgerbutton.js
+++
@@ -1,8 +0,0 @@
-    document.addEventListener('DOMContentLoaded', () => {
-      const burger = document.querySelector('.navbar-burger');
-      const menu = document.getElementById('navbarBasic');
-      burger.addEventListener('click', () => {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-      });
-    });
