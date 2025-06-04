0cf7f8e7143531b09e17edaea3aed4f7b231e2ee
ndamatta <damatta.natanael@gmail.com>
2025-06-04 19:26:42 -0300

    add separators and smoke test for comparator funcionality, vague mockup design

--- public/js/main-js.js
+++ public/js/main-js.js
@@ -1,3 +1,4 @@
+// Burger button functionality
 document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.navbar-burger');
   const menu = document.getElementById(burger.dataset.target);
@@ -8,4 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+//Comparator functionality
+function startComparator () {
+const followers_list = document.getElementById('followers').value
+console.log(followers_list);
+}
+
+document.getElementById('textarea-button').addEventListener('click', startComparator)
+
+
+ // Copyright year update
 document.getElementById('year').textContent = new Date().getFullYear();
+
+
