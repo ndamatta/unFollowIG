f530689a7d97ab0826b539d0fa67c42ac73f5888
ndamatta <damatta.natanael@gmail.com>
2025-06-12 15:57:16 -0300

    improve semmantic for better styiling, now each user is placed inside a list of unfollowers/accounts

--- public/js/showUnfollowers.js
+++ public/js/showUnfollowers.js
@@ -1,20 +1,21 @@
 function showUnfollowers(unfollowers) {
   const container = document.getElementById("unfollowers");
 
-  if (unfollowers.length == 0) {
-    let html = `<h2>You don't have any unfollowers</h2>`;
+  if (unfollowers.length === 0) {
+    const html = `<h2 class="success">You don't have any unfollowers</h2>`;
     container.innerHTML = html;
-    
   } else {
     let html = `<h2>You have ${unfollowers.length} unfollowers</h2>`;
+    html += `<ul id="unfollowersAccounts">`;
+
     unfollowers.forEach((user) => {
       html += `
-            <a href="https://www.instagram.com/${user}" target="_blank">
-                ${user}
-            </a><br>
-            `;
+        <li>
+          <a href="https://www.instagram.com/${user}" target="_blank">${user}</a>
+        </li>`;
     });
 
+    html += `</ul>`;
     container.innerHTML = html;
   }
 }
