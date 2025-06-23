434f6a0eb99b671c0c2de113263ebdf2e69469d2
ndamatta <damatta.natanael@gmail.com>
2025-06-23 20:22:53 -0300

    add 404.html and fix path to source

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+    res.status(404).sendFile(path.join(__dirname, '../views/404.html'));
 });
 
 // Start the server
