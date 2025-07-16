92552f2f1cc632fe7d6f42a650ddac1f3fea8168
ndamatta <damatta.natanael@gmail.com>
2025-07-15 23:18:09 -0300

    increase json parser limit to avoid 413 error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(express.static('public'));
 
 // Check env
